642dad91671c171068fb0caaa9df99fc83943528
yuyang <yuyang@yuyangs-MacBook-Pro.local>
2017-08-21 10:50:23 +0800

    add sougou quesiton fetcher

---
+++ oneline_questions.py
@@ -0,0 +1,21 @@
+
+from Utility import URLUtility
+from bs4 import BeautifulSoup
+import socket
+socket.setdefaulttimeout(5)
+
+def get_questions_from_sougou():
+    fw = open('input/sougou.csv', 'w')
+
+    for idx in range(1, 500):
+        print(idx)
+        url = 'http://wenwen.sogou.com/cate/tag?tag_id=0&tp=0&pno=%d&ch=ww.fly.fy%d#questionList' % (idx, idx)
+        succeed, content = URLUtility.get_response_content_for_url(url, 'utf8')
+        if succeed:
+            soup = BeautifulSoup(content)
+            for elem in soup.select("ul.sort-lst a p"):
+                fw.write(elem.text + '\n')
+    fw.close()
+
+
+get_questions_from_sougou()
