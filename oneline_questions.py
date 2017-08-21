7ea15a02f70297ff7cc59ab06c59a1e600e75c27
yuyang <yuyang@yuyangs-MacBook-Pro.local>
2017-08-21 11:21:54 +0800

    test the most similar questions: the result is not good

--- oneline_questions.py
+++ oneline_questions.py
@@ -7,7 +7,7 @@ socket.setdefaulttimeout(5)
 def get_questions_from_sougou():
     fw = open('input/sougou.csv', 'w')
 
-    for idx in range(1, 500):
+    for idx in range(1, 1000):
         print(idx)
         url = 'http://wenwen.sogou.com/cate/tag?tag_id=0&tp=0&pno=%d&ch=ww.fly.fy%d#questionList' % (idx, idx)
         succeed, content = URLUtility.get_response_content_for_url(url, 'utf8')
--- similar_test.py
+++ similar_test.py
@@ -7,11 +7,12 @@ import random
 import gensim.utils
 import gensim.models.doc2vec
 import jieba
+import sys
 
 
-def read_corpus(fname):
+def read_corpus(fname, start_idx = -1):
 
-    idx = -1
+    idx = start_idx
     with open(fname) as f:
         for i, line in enumerate(f):
             line_parts = gensim.utils.simple_preprocess(line)
@@ -26,14 +27,18 @@ def read_corpus(fname):
             yield gensim.models.doc2vec.TaggedDocument(parts, [idx])
 
 
-train_corpus = list(read_corpus('input/questions.csv'))
+train_corpus_1 = list(read_corpus('input/questions.csv'))
+train_corpus_2 = list(read_corpus('input/sougou.csv', start_idx=len(train_corpus_1)))
 
+# train_corpus.extend(list(read_corpus('input/sougou.csv')))
 
-
+total_corpus = []
+total_corpus.extend(train_corpus_1)
+total_corpus.extend(train_corpus_2)
 model = gensim.models.doc2vec.Doc2Vec(size=600, min_count=1, iter=55)
-model.build_vocab(train_corpus)
+model.build_vocab(total_corpus)
 
-model.train(train_corpus, total_examples=model.corpus_count, epochs=model.iter)
+model.train(total_corpus, total_examples=model.corpus_count, epochs=model.iter)
 
 ranks = []
 
@@ -42,7 +47,15 @@ second_ranks = []
 correct = 0
 wrong = 0
 
-for words, doc_id in train_corpus:
+#build the doc dictionary
+doc_dic = {}
+for words, doc_ids in train_corpus_1:
+    doc_dic[doc_ids[0]] = words
+
+for words, doc_ids in train_corpus_2:
+    doc_dic[doc_ids[0]] = words
+
+for words, doc_id in train_corpus_1:
     doc_id = doc_id[0]
     # try:
     inferred_vector = model.infer_vector(words)
@@ -52,7 +65,9 @@ for words, doc_id in train_corpus:
     # print('most similar index', sims[0][0])
     print('pairs')
     print(words)
-    print('most similar docs', train_corpus[sims[0][0]])
+
+    print('most similar docs', doc_dic[sims[0][0]])
+    print('second similar docs', doc_dic[sims[1][0]])
 
     if doc_id == sims[0][0]:
         correct += 1
